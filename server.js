ad074d7a69f74ee05ea27be6d57da4bc71bb0e9d
roy-ca <royca98@gmail.com>
2018-02-21 19:51:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'aArticle1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
